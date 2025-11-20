f3d6266527014c3bdef13ed3109f299c666eb521
fresita18 <156115923+fresita18@users.noreply.github.com>
2025-11-19 21:32:45 -0500

    .

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -20,14 +20,14 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.smart_break"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+
+        // FIX: Kotlin DSL requires .set()
+        versionCode.set(flutter.versionCode.toInt())
+        versionName.set(flutter.versionName)
     }
 
     buildTypes {
